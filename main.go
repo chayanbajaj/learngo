7ab766a84eb02757ea74693922a138bc9d1a4393
chayanbajaj <chayanbajaj@gmail.com>
2022-08-31 18:59:03 +0530

    slice-data-structure-2

--- main.go
+++ main.go
@@ -6,14 +6,14 @@ func main() {
 	//In Go we don't use arrays, we use Slice
 	// var sliceName []dataType
 
-	var mySlice []string
+	var mySlice []int
 
 	//To add an element in slice we use append method
 	// sliceName = append(sliceName, value)
 
-	mySlice = append(mySlice, "John")
-	mySlice = append(mySlice, "Trevor")
-	mySlice = append(mySlice, "Mary")
+	mySlice = append(mySlice, 2)
+	mySlice = append(mySlice, 1)
+	mySlice = append(mySlice, 3)
 
 	log.Println(mySlice)
 }
