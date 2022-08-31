337eb0070166aa65de180761ffe1c8718bd5e652
chayanbajaj <chayanbajaj@gmail.com>
2022-08-31 15:12:11 +0530

    variable-1

--- main.go
+++ main.go
@@ -3,6 +3,9 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("Hello, World.")
-	//run using go run main.go
+	var whatToSay string
+	// var var_name data_type
+
+	whatToSay = "Goodbye, cruel world!"
+	fmt.Println(whatToSay)
 }
