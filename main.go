7b315197ae05f7514ccc5823d98d369a0f9bdcdf
chayanbajaj <chayanbajaj@gmail.com>
2022-08-31 18:44:50 +0530

    map-data-structure-2

--- main.go
+++ main.go
@@ -6,16 +6,10 @@ func main() {
 	// Common Syntax to make map
 	// varName := make(map[keyDataType]dataType)
 
-	myMap := make(map[string]string)
-	myMap["dog"] = "MoolchandJi"
-	myMap["cat"] = "Cassie"
+	myMap := make(map[string]int)
+	myMap["First"] = 1
+	myMap["Second"] = 2
 
-	log.Println(myMap["dog"])
-	log.Println(myMap["cat"])
-
-	//We can also override using key
-
-	myMap["dog"] = "Samson"
-
-	log.Println(myMap["dog"])
+	log.Println(myMap["First"])
+	log.Println(myMap["Second"])
 }
