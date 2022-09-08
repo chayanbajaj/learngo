28f61ef753b3e8f3cdb6b4b856985f2759f9188b
chayanbajaj <chayanbajaj@gmail.com>
2022-09-08 12:38:37 +0530

    if..else..-1

--- main.go
+++ main.go
@@ -1,19 +1,16 @@
 package main
 
-import (
-	"log"
-)
+import "log"
 
 func main() {
-	//We can declare and initialise in a slice as
-	// varName := []dataType{values}
 
-	numbers := []int{1, 2, 3, 4, 5, 6, 7, 8, 9}
+	var isTrue bool
 
-	log.Println(numbers)
+	isTrue = true
 
-	//We can also print a range of slice
-
-	log.Println(numbers[0:2])
-	log.Println(numbers[5:8])
+	if isTrue {
+		log.Println("isTrue is", isTrue)
+	} else {
+		log.Println("isTrue is", isTrue)
+	}
 }
