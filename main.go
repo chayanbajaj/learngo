b7ed7733d2be52b7cd43d083c65f55482fa3e99c
chayanbajaj <chayanbajaj@gmail.com>
2022-09-08 12:59:58 +0530

    for loop - 1

--- main.go
+++ main.go
@@ -3,22 +3,7 @@ package main
 import "log"
 
 func main() {
-	myVar := "cat"
-
-	switch myVar {
-	case "cat":
-		log.Println("cat is set to cat")
-
-	case "dog":
-		log.Println("cat is set to dog")
-
-	case "cow":
-		log.Println("cat is set to cow")
-
-	case "fish":
-		log.Println("cat is set to fish")
-
-	default:
-		log.Println("cat is set to something else")
+	for i := 0; i <= 10; i++ {
+		log.Println(i)
 	}
 }
