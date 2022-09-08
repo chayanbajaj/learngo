7fb4f9168dbc6b4712ed67d172adf071e1d9951d
chayanbajaj <chayanbajaj@gmail.com>
2022-09-08 17:41:54 +0530

    channel

--- main.go
+++ main.go
@@ -6,12 +6,19 @@ import (
 	"github.com/youruser/yourrepo/helpers"
 )
 
-func main() {
-	var myVar helpers.SomeType
-	myVar.TypeName = "SomeName"
-	log.Println(myVar.TypeName)
-}
+const numPool = 10
 
-//To create a package firstly go in the root level of the directory and run command go mod init github.com/chayanbajaj/projectname
-//Then in the root level directory create a folder with the name of package
-//inside the created folder place the file with the same name as of the folder followed by .go extension
+func calculateValue(intChan chan int) {
+	randomNumber := helpers.RandomNumber(numPool)
+	intChan <- randomNumber
+}
+func main() {
+	intChan := make(chan int)
+	defer close(intChan) //defer is executed after the completion of current function
+
+	//running a function with go routine
+	go calculateValue(intChan)
+
+	num := <-intChan
+	log.Println(num)
+}
