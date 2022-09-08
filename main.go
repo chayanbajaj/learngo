6c01ce4413670d9c63ae2ff45d97b45c232c4cd2
chayanbajaj <chayanbajaj@gmail.com>
2022-09-08 13:08:50 +0530

    for loop - 3

--- main.go
+++ main.go
@@ -4,9 +4,10 @@ import "log"
 
 func main() {
 
-	animals := []string{"cat", "dog", "fish", "horse", "buffalo"}
+	animals := make(map[string]string)
+	animals["dog"] = "Fido"
+	animals["cat"] = "Fluffy"
 
-	// if we don't care about the index i then we can use blank identifier in place of that
 	for _, animal := range animals {
 		log.Println(animal)
 	}
